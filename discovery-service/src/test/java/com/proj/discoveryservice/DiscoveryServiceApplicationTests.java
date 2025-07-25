43e30f98ac8f4f0fdf10e754b83e2736bd1815ed
Akram BENHAMMOU <aben200244@gmail.com>
2025-07-25 19:40:35 +0200

    init: add configuration and dicovery services

---
+++ discovery-service/src/test/java/com/proj/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
