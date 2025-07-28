96d877af11628689f40ffdbae5e4933e60692919
Akram BENHAMMOU <aben200244@gmail.com>
2025-07-28 02:25:11 +0200

    feat(dynamic way) : config routes and discovery service

--- gateway-service/src/main/java/com/proj/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/proj/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,12 @@ package com.proj.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.config.GatewayProperties;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionRouteLocator;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +16,9 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator loc(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc ,dlp);
+    }
 }
