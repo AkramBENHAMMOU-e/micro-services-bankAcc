9225026aa7db51fcd62f7c8e1942aee65ca17ca8
Akram BENHAMMOU <aben200244@gmail.com>
2025-07-29 05:08:55 +0200

    feat: add Resilience4j circuit breaker on CustomerRestClient call

--- account-service/src/main/java/com/proj/accountservice/clients/CustomerRestClient.java
+++ account-service/src/main/java/com/proj/accountservice/clients/CustomerRestClient.java
@@ -1,6 +1,7 @@
 package com.proj.accountservice.clients;
 
 import com.proj.accountservice.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,14 @@ import java.util.List;
 @FeignClient(name = "CUSTOMER-SERVICE")
 public interface CustomerRestClient {
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerService" ,fallbackMethod = "getDefaultCustomer")
     Customer findCustomerById(@PathVariable Long id);
+
+    default  Customer getDefaultCustomer(Long id, Exception exception){
+
+        return  Customer.builder().id(id).firstName("Not avilable").lastName("Not available").build();
+    }
+
     @GetMapping("/customers")
     List<Customer> allCustomers();
 }
--- account-service/src/main/java/com/proj/accountservice/model/Customer.java
+++ account-service/src/main/java/com/proj/accountservice/model/Customer.java
@@ -1,9 +1,10 @@
 package com.proj.accountservice.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @Builder
 public class Customer {
     private Long id;
     private String firstName;
